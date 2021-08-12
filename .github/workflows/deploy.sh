6ceb7f4368f112b33bbc4affaddac48302983c10
Duarte Santos <dsantos@users.noreply.github.com>
2021-08-12 12:09:15 +0100

    auto accept

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-sudo apt-get install apt-transport-https --yes
+sudo apt-get -y install apt-transport-https --yes
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
+sudo apt-get -y update
+sudo apt-get -y install helm
 
 helm version
 
