9e06ec9c6f97de2647659494c6c80a686932db51
Duarte Santos <dsantos@users.noreply.github.com>
2021-08-12 12:41:56 +0100

    use helm template

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,9 +6,19 @@ ENVS=$(echo $1 | jq -r '.labels[] | select(.name | startswith("env-")).name')
 SERVICES=$(echo $1 | jq -r '.labels[] | select(.name | startswith("svc-")).name')
 DOCKER_IMAGE=$(echo $1 | jq -r '.tags')
 
-echo $ENVS
-echo $SERVICES
-echo $DOCKER_IMAGE
+for env in $ENVS
+do
+  for svc in $SERVICES
+  do
+    SERVICE_PATH=nonprod/$env/services/$svc
+    echo "processing $SERVICE_PATH"
+
+    mkdir -p $SERVICE_PATH
+    helm template charts/application --set service.name=$svc --set service.image=$DOCKER_IMAGE > $SERVICE_PATH/app.yaml
+
+    ls -lhart $SERVICE_PATH
+  done
+done
 
 ## @TODO support "all-env" "all-app" ect
 #for label in $FILTEREDLABELS
