87cc12a685b8d4291b403e887f60c5b94b369700
Duarte Santos <dsantos@users.noreply.github.com>
2021-08-12 12:24:55 +0100

    parsing

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,25 +8,26 @@ sudo apt-get -y install helm
 
 helm version
 
-FILTEREDLABELS=$(echo $1 | jq '.labels' | jq -r '.[] | .name ')
+ENVS=$(cat $1 | jq -r '.labels[] | select(.name | startswith("env-")).name')
+SERVICES=$(cat $1 | jq -r '.labels[] | select(.name | startswith("svc-")).name')
+DOCKER_IMAGE=$(echo $1 | jq -r '.tags')
 
-DOCKER_IMAGE=$(echo $1 | jq '.tags')
+echo $ENVS
+echo $SERVICES
+echo $DOCKER_IMAGE
 
-APPS=()
-NAMESPACES=()
-
-# @TODO support "all-env" "all-app" ect
-for label in $FILTEREDLABELS
-do
-    if [[ $label == "env-"* ]]; then
-      NAMESPACES+=($label)
-    fi
-
-    if [[ $label == "svc-"* ]]; then
-      APPS+=($label)
-    fi
-done
-
-echo "docker-image: $DOCKER_IMAGE"
-echo "namespaces: ${NAMESPACES[@]}"
-echo "apps: ${APPS[@]}"
+## @TODO support "all-env" "all-app" ect
+#for label in $FILTEREDLABELS
+#do
+#    if [[ $label == "env-"* ]]; then
+#      NAMESPACES+=($label)
+#    fi
+#
+#    if [[ $label == "svc-"* ]]; then
+#      APPS+=($label)
+#    fi
+#done
+#
+#echo "docker-image: $DOCKER_IMAGE"
+#echo "namespaces: ${NAMESPACES[@]}"
+#echo "apps: ${APPS[@]}"
