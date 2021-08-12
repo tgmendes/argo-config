284f15b6f7b25a7cc34d0b9bad723c863233b803
Duarte Santos <dsantos@users.noreply.github.com>
2021-08-12 12:35:31 +0100

    install helm as a step

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-sudo apt-get -y install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get -y update
-sudo apt-get -y install helm
-
 helm version
 
 ENVS=$(echo $1 | jq -r '.labels[] | select(.name | startswith("env-")).name')
